96135db940e25ccc5316951acc72d3c0c8b0b927
Joelton <114880776+joeltonken@users.noreply.github.com>
2024-10-01 21:54:11 -0300

    standardizing returns with a responseDTO

--- src/main/java/com/estudos/br/pdv/controllers/SaleController.java
+++ src/main/java/com/estudos/br/pdv/controllers/SaleController.java
@@ -1,6 +1,8 @@
 package com.estudos.br.pdv.controllers;
 
+import com.estudos.br.pdv.dtos.ResponseDTO;
 import com.estudos.br.pdv.dtos.SaleDTO;
+import com.estudos.br.pdv.dtos.SaleInfoDTO;
 import com.estudos.br.pdv.exceptions.InvalidOperationException;
 import com.estudos.br.pdv.exceptions.NoItemFoundException;
 import com.estudos.br.pdv.services.SaleService;
@@ -8,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/sale")
@@ -18,13 +22,13 @@ public class SaleController {
 
     @GetMapping
     public ResponseEntity getAll() {
-        return new ResponseEntity<>(saleService.findAll(), HttpStatus.OK);
+        return new ResponseEntity<>(new ResponseDTO<>("", saleService.findAll()), HttpStatus.OK);
     }
 
     @GetMapping("{id}")
     public ResponseEntity getById(@PathVariable Long id) {
         try {
-            return new ResponseEntity<>(saleService.getById(id), HttpStatus.OK);
+            return new ResponseEntity<>(new ResponseDTO<>("", saleService.getById(id)), HttpStatus.OK);
         } catch (NoItemFoundException | InvalidOperationException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
@@ -34,11 +38,11 @@ public class SaleController {
     public ResponseEntity post(@RequestBody SaleDTO saleDTO) {
         try {
             long id = saleService.save(saleDTO);
-            return new ResponseEntity<>("Venda realizada com sucesso: " + id, HttpStatus.CREATED);
-        } catch (NoItemFoundException | InvalidOperationException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new ResponseDTO<>("Venda realizada com sucesso!", id), HttpStatus.CREATED);
+        } catch (NoItemFoundException | InvalidOperationException error) {
+            return new ResponseEntity<>(new ResponseDTO<>(error.getMessage(), null), HttpStatus.BAD_REQUEST);
         } catch (Exception error) {
-            return new ResponseEntity<>(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new ResponseDTO<>(error.getMessage(), null), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
