d457b6d4110edfe23a2cbcebb756e2fddc80268e
Joelton <114880776+joeltonken@users.noreply.github.com>
2024-10-18 23:52:15 -0300

    security route test

--- src/main/java/com/estudos/br/pdv/security/SecurityConfig.java
+++ src/main/java/com/estudos/br/pdv/security/SecurityConfig.java
@@ -34,6 +34,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                "/info"
+                        ).permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(withDefaults()) // Use a configuração padrão para autenticação básica
                 .csrf(csrf -> csrf.disable()); // Desabilita CSRF
