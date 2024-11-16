592c3f9ae61c9ffa720149d869d05f701a9f4af1
Joelton <114880776+joeltonken@users.noreply.github.com>
2024-11-16 12:15:06 -0300

    fix swagger config

--- src/main/java/com/estudos/br/pdv/config/SpringDocOpenApiConfig.java
+++ src/main/java/com/estudos/br/pdv/config/SpringDocOpenApiConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,13 +22,21 @@ public class SpringDocOpenApiConfig {
                                 .description("API de Ponto de Venda - PDV")
                                 .version("v1")
                                 .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0"))
-                                .contact(new Contact().name("Joelton Gomes").email("joeken.jp@gmail.com"))
-                );
-//                .components(new Components()
-//                                .addSecuritySchemes("BEARER ", new SecurityScheme()
-//                                        .type(SecurityScheme.Type.HTTP)
-//                                        .scheme("BEARER")
-//                                        .bearerFormat("JWT")));
+                                .contact(new Contact().name("Joelton Gomes").email("joeken.jp@gmail.com")))
+                .addSecurityItem(new SecurityRequirement().addList("JWT"))
+                .components(new Components()
+                                .addSecuritySchemes("JWT", createJWT()));
     }
+
+    private SecurityScheme createJWT() {
+        return new SecurityScheme()
+                .name("JWT")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+    }
+
 
 }
--- src/main/java/com/estudos/br/pdv/security/SecurityConfig.java
+++ src/main/java/com/estudos/br/pdv/security/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.estudos.br.pdv.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -18,12 +18,17 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Autowired
-    private CustomUserDetailsService service;
-    @Autowired
-    private JWTService jwtService;
+    private final CustomUserDetailsService service;
+
+    private final JWTService jwtService;
+
+    // http://localhost:8080/swagger-ui/index.html
+    private static final String[] AUTH = { "/api/pessoas/**",
+            "/login", "/sign-up", "/v3/api-docs/**",
+            "/swagger-ui/**", "/swagger-ui.html","/swagger-resources/**", "/webjars/**" };
 
     @Bean
     public static PasswordEncoder passwordEncoder(){
@@ -34,10 +39,7 @@ public class SecurityConfig {
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST,"/users").permitAll()
-                        .requestMatchers("/sign-up").permitAll()
-                        .requestMatchers("/login").permitAll()
-                        //.requestMatchers("").permitAll()
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                        .requestMatchers(AUTH).permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(withDefaults()) // Use a configuração padrão para autenticação básica
                 .sessionManagement((session)-> session
